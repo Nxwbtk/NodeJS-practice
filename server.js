76a9fc4dc941577f8092c39f10dc74cea33bf22e
nxwbtk <buntakan0703@gmail.com>
2024-04-28 13:45:29 +0000

    Add start and dev scripts to package.json, and update server.js to return JSON response

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var http = require('http');
 
 const server = http.createServer((req, res) => {
-	res.end('Hello World!');
+	// res.setHeader('Content-Type', 'text/html');
+	// res.statusCode = 200;
+	res.writeHead(200, { 'Content-Type': 'application/json' });
+	res.end(JSON.stringify({ message: 'Hello World' }));
 });
 
 server.listen(8000, () => {
